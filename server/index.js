1fa750f5255df9a5f74621c84f67833c969d06a8
Bonilla7784 <a00826077@itesm.mx>
2022-10-07 10:23:51 -0500

    Connection to DB

--- server/index.js
+++ server/index.js
@@ -2,16 +2,31 @@ const express = require("express");
 const mysql = require("mysql")
 
 const app = express();
+const port = process.env.PORT;
 
 app.use(express.json());
 
-const dbTest = mysql.createConnection({
-    user: "root",
-    host: "localhost",
-    password: "Onfire77..",
-    database: "LoginSystem",
+require("dotenv").config();
+
+const db_Host = process.env.db_host;
+const db_User = process.env.db_user;
+const db_Password = process.env.db_password;
+const db_Database = process.env.db_database;
+const db_Port = process.env.db_port;
+
+const dbTest = mysql.createPool({
+    connectionLimit: 10,
+    user: db_User,
+    host: db_Host,
+    password: db_Password,
+    database: db_Database,
+    port: db_Port
 });
 
-app.listen(4000, () => {
-    console.log("Running");
+dbTest.getConnection((err, connection) => {
+    if (err) throw (err)
+    console.log("Database connected succesful: " + connection.threadId);
 });
+
+app.listen(port,
+    () => console.log(`Server startated on port ${port}...`));
