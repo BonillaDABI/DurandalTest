e80f27e04138edf498fc6ab7015fc42fc52fc3a3
Bonilla7784 <115091301+BonillaDABI@users.noreply.github.com>
2022-10-11 11:39:16 -0500

    BackEnd update rdy for testing

--- server/index.js
+++ server/index.js
@@ -8,22 +8,58 @@ app.use(express.json());
 
 require('dotenv').config()
 
-console.log("Host: " + process.env.HOST);
 
-/*
 const dbTest = mysql.createPool({
     connectionLimit: 10,
-    user: db_User,
-    host: db_host,
-    password: db_password,
-    database: db_database,
-    port: db_port
-});*/
+    user: "root",
+    host: "localhost",
+    password: "Onfire77..",
+    database: "login",
+    port: "3306"
+});
 
 dbTest.getConnection((err, connection) => {
     if (err) throw (err)
     console.log("Database connected succesful: " + connection.threadId);
 });
 
+app.post("/register", (req, res) => {
+
+    const uName = req.body.user_name;
+    const user_password = req.body.user_password;
+    const first_surname = req.body.first_surname;
+    const second_surname = req.body.second_surname;
+    const email = req.body.email;
+
+    dbTest.query("INSERT INTO users (name, first_surname, second_surname, email, password) VALUES(?, ?, ?, ?, ?)",
+        [uName, first_surname, second_surname, email, user_password],
+        (err, result) => {
+            console.log(err);
+        }
+    );
+});
+
+
+app.post("/login", (req, res) => {
+    const uName = req.body.user_name;
+    const user_password = req.body.user_password;
+
+    dbTest.query(
+        "SELECT * FROM users WHERE  username = ? AND password = ?",
+        [uName, user_password],
+        (err, result) => {
+            if (err) {
+                res.send({ err: err });
+            }
+
+            if (result.length > 0) {
+                res.send(result);
+            } else {
+                res.send({ message: "Combinacion de usario y contraseña equivocada." });
+            }
+        }
+    );
+});
+
 app.listen(port,
-    () => console.log(`Server startated on port ${port}...`));
+    () => console.log(`Server startated on port ` + port));
