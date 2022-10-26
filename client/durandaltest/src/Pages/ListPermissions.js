cc3393f8eb8e183b8fd739bc3c0966256e6ca23b
Miguel Ángel Bermea Rdz <a01411671@itesm.mx>
2022-10-26 14:46:08 -0500

    ListPermissions and ListRoles frontend

--- client/durandaltest/src/Pages/ListPermissions.js
+++ client/durandaltest/src/Pages/ListPermissions.js
@@ -1,13 +1,55 @@
-//import React, { useState } from "react";
-//import axios from "axios";
+import {React, useEffect, useState } from "react";
+import axios from "axios";
 import NavBar from "../Components/Navbar";
 
-const ListPermissions = () => { 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
+
+function ListPermissions() { 
+  const [data, setData] = useState([]);
+
+  const getData = () => {
+    axios.get('http://localhost:3001/permissions')
+      .then((response) => {
+        console.log(response.data);
+        setData(response.data); 
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    };
+
+    useEffect(() => {
+      getData();
+    }, []);
+      
+    const deleteData = () => {
+      
+    }
 
     return (
       <div className="ListPermissions-container">
         <NavBar />
-        <h1>ListPermissions Test</h1>
+        <h1>ListRoles Test</h1>
+        <tbody className="users-table">
+          <tr className="head-row">
+            <th>Id</th>
+            <th>Permission</th>
+            <th>Delete</th>
+          </tr>
+          {data.map((item, index) => (
+            <tr key={index} className="table-rows">
+              <td>{item.id}</td>
+              <td>{item.per_name}</td>
+              <td>
+                <button>
+                  <FontAwesomeIcon icon={faTrashCan} />
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </div>
     );
   }
