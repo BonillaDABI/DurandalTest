fbdc920a0fd42f6ac57b837fa8b31a9db41ca45d
Miguel Ángel Bermea Rdz <a01411671@itesm.mx>
2022-11-30 15:58:29 -0600

    ClientInfo Page with tabs init

--- client/durandaltest/src/Pages/ClientInfo.js
+++ client/durandaltest/src/Pages/ClientInfo.js
@@ -1,17 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Sidebar from "../Components/Sidebar/Sidebar";
 import Navbar from "../Components/Navbar/Navbar";
 
 import "../SCSS/Elements/_tables.scss"
+import { Box, Tab, Tabs } from '@mui/material';
+
+
 
 function ClientInfo() {
+  var business_name = localStorage.getItem("business_name");
+  var client_id = localStorage.getItem("client_id");
+
+  const [tabIndex, setTabIndex] = useState(0);
+ 
+  const handleTabChange = (event, newTabIndex) => {
+    setTabIndex(newTabIndex);
+  };
+
   return (
     <div className="list">
       <Sidebar />
       <div className="listContainer">
         <Navbar />
-        <h1>Nombre del cliente</h1>
+        <h1>{business_name}</h1>
+        <Box>
+          <Box>
+            <Tabs value={tabIndex} onChange={handleTabChange}>
+              <Tab label="Contactos"></Tab>
+              <Tab label="Sitios"></Tab>
+            </Tabs>
+          </Box>
+          <Box sx={{ padding: 2 }}>
+          {tabIndex === 0 && (
+            <Box>
+              The first tab
+            </Box>
+          )}
+          {tabIndex === 1 && (
+            <Box>
+              The second tab
+            </Box>
+          )}
+          </Box>
+        </Box>
       </div>
     </div>
   );
