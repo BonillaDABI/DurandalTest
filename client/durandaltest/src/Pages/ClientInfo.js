0e5916d464416d0e0188a9a74c3ca1f72267d2a4
Miguel Ángel Bermea Rdz <a01411671@itesm.mx>
2022-12-02 02:49:30 -0600

    Client update, PENDING design - scss

--- client/durandaltest/src/Pages/ClientInfo.js
+++ client/durandaltest/src/Pages/ClientInfo.js
@@ -14,6 +14,33 @@ import axios from "axios";
 
 function ClientInfo() {    
   const [tabIndex, setTabIndex] = useState(0);
+  const [business, setBusiness] = useState("");
+  const [rfc, setRFC] = useState("");
+  const [taxId, setTaxId] = useState("");
+
+  var clients = JSON.parse(localStorage.getItem("clients"));
+
+  function updateClient() {
+    var updateClientId = localStorage.getItem("client_id");
+    axios.patch(`http://localhost:3001/updateClient/${updateClientId}`, { // url to POST
+      'Authorization': "bearer " + localStorage.getItem('token'),
+      //user_name: userName,
+      business_name: business,
+      rfc: rfc,
+      tax_id: taxId
+    },)
+      .then((response) => {
+        console.log(response);
+        alert("Cliente actualizado exitosamente en la base de datos.");
+        localStorage.setItem("business_name", business);
+        localStorage.setItem("client_rfc", rfc);
+        localStorage.setItem("client_tax_id", taxId);
+        window.location.reload();
+      }, (error) => {
+        console.log(error);
+        alert("Error al actualizar datos del cliente. Vuelve a intentarlo.");
+      });
+  }
  
   const handleTabChange = (event, newTabIndex) => {
     setTabIndex(newTabIndex);
@@ -29,9 +56,39 @@ function ClientInfo() {
       <Sidebar />
       <div className="listContainer">
         <Navbar />
-        <span><strong>{businessName}</strong></span><br />
-        <span>{clientRFC}</span><br />
-        <span>{clientTax}</span>
+        <div className="clientinfo-container">
+          <span><strong>{businessName}</strong></span><br />
+          <span>{clientRFC}</span>
+        </div>
+        <form>
+          <div className='form-fields' id="large-form-field">
+            <div className='mini-input-container'>
+              <span className="input-span">Nombre del negocio</span>
+              <input className="input-field" type="text" placeholder="Ingresar..." value={business} onChange={(e) => setBusiness(e.target.value)} required />
+            </div>
+          </div>
+          <div className='form-fields'>
+            <div className='mini-input-container'>
+              <span className="input-span">RFC</span>
+              <input className="input-field" type="text" placeholder="Ingresar..." value={rfc} onChange={(e) => setRFC(e.target.value)} required />
+            </div>
+          </div>
+          <div className='form-fields'>
+            <div className='mini-input-container'>
+              <span className="input-span">Tax ID</span>
+              <select className="input-field"  value={taxId} onChange={(e) => setTaxId(e.target.value)} required>
+                <option value="" disabled hidden className="options">Seleccionar...</option> 
+                {clients.activeTaxes.map((item, i) => {
+                  return <option className="options" key={i} value={item.id}>{item.name}</option>
+                })};
+              </select>
+            </div>
+          </div>        
+        </form>
+        <div className="button-container">
+          <button className='save-button' onClick={() => {updateClient();}}>Guardar cambios</button>
+        </div>
+        <div className='divider'></div>
         <Box>
           <Box>
             <Tabs 
