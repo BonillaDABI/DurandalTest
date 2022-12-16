cf83e9c2db48fdd9315c829466c2fd294a7148c7
Miguel Ángel Bermea Rdz <a01411671@itesm.mx>
2022-12-16 10:29:45 -0600

    Small cleanup of frontend

--- client/durandaltest/src/Pages/Create.js
+++ client/durandaltest/src/Pages/Create.js
@@ -1,118 +1,118 @@
-import React, { useState } from "react";
-//import { useNavigate } from "react-router-dom";
-import axios from "axios";
-import NavBar from "../Components/Navbar";
+// import React, { useState } from "react";
+// //import { useNavigate } from "react-router-dom";
+// import axios from "axios";
+// import NavBar from "../Components/Navbar";
 
-const Create = () => {
-  //const navigate = useNavigate();
-  //const [userName, setUser] = useState("");
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const [firstSurname, setFirstSurname] = useState("");
-  const [secondSurname, setSecondSurname] = useState("");
-  const [password, setPW] = useState("");
+// const Create = () => {
+//   //const navigate = useNavigate();
+//   //const [userName, setUser] = useState("");
+//   const [email, setEmail] = useState("");
+//   const [name, setName] = useState("");
+//   const [firstSurname, setFirstSurname] = useState("");
+//   const [secondSurname, setSecondSurname] = useState("");
+//   const [password, setPW] = useState("");
 
-  const createUser = () => {
-    axios.post('http://localhost:3001/register', { // url to POST
-      //user_name: userName,
-      'Authorization': "bearer " + localStorage.getItem('token'),
-      name: name,
-      first_surname: firstSurname,
-      second_surname: secondSurname,
-      email: email,
-      password: password
+//   const createUser = () => {
+//     axios.post('http://localhost:3001/register', { // url to POST
+//       //user_name: userName,
+//       'Authorization': "bearer " + localStorage.getItem('token'),
+//       name: name,
+//       first_surname: firstSurname,
+//       second_surname: secondSurname,
+//       email: email,
+//       password: password
 
-    })
-      .then((response) => {
-        console.log(response);
-        alert("Usuario creado exitosamente en la base de datos.");
-      }, (error) => {
-        console.log(error);
-        alert("Error al crear usuario. Vuelve a intentarlo.");
-      });
-  }
+//     })
+//       .then((response) => {
+//         console.log(response);
+//         alert("Usuario creado exitosamente en la base de datos.");
+//       }, (error) => {
+//         console.log(error);
+//         alert("Error al crear usuario. Vuelve a intentarlo.");
+//       });
+//   }
 
-  /*
-  const onSuccess = (res) => {
-    console.log('success:', res);
-  };
-  const onFailure = (err) => {
-    console.log('failed:', err);
-  };
-  */
+//   /*
+//   const onSuccess = (res) => {
+//     console.log('success:', res);
+//   };
+//   const onFailure = (err) => {
+//     console.log('failed:', err);
+//   };
+//   */
 
-  return (
-    <div className="cu-container">
-      <NavBar />
-      <div className="cu-content-container">
-        <form className="cu-form">
-          <h1 className="title">DABI Durandal | Create - Users</h1>
-          <div className="input-container">
-            <label className="input-label">Name</label>
-            <input
-              type="text"
-              className="input-field"
-              placeholder="e.g Miguel"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </div>
+//   return (
+//     <div className="cu-container">
+//       <NavBar />
+//       <div className="cu-content-container">
+//         <form className="cu-form">
+//           <h1 className="title">DABI Durandal | Create - Users</h1>
+//           <div className="input-container">
+//             <label className="input-label">Name</label>
+//             <input
+//               type="text"
+//               className="input-field"
+//               placeholder="e.g Miguel"
+//               value={name}
+//               onChange={(e) => setName(e.target.value)}
+//             />
+//           </div>
 
-          <div className="input-container">
-            <label className="input-label">First Surname</label>
-            <input
-              type="text"
-              className="input-field"
-              placeholder="e.g Bermea"
-              value={firstSurname}
-              onChange={(e) => setFirstSurname(e.target.value)}
-            />
-          </div>
+//           <div className="input-container">
+//             <label className="input-label">First Surname</label>
+//             <input
+//               type="text"
+//               className="input-field"
+//               placeholder="e.g Bermea"
+//               value={firstSurname}
+//               onChange={(e) => setFirstSurname(e.target.value)}
+//             />
+//           </div>
 
-          <div className="input-container">
-            <label className="input-label">Second Surname</label>
-            <input
-              type="text"
-              className="input-field"
-              placeholder="e.g Rodriguez"
-              value={secondSurname}
-              onChange={(e) => setSecondSurname(e.target.value)}
-            />
-          </div>
+//           <div className="input-container">
+//             <label className="input-label">Second Surname</label>
+//             <input
+//               type="text"
+//               className="input-field"
+//               placeholder="e.g Rodriguez"
+//               value={secondSurname}
+//               onChange={(e) => setSecondSurname(e.target.value)}
+//             />
+//           </div>
 
-          <div className="input-container">
-            <label className="input-label">Email address</label>
-            <input
-              type="email"
-              className="input-field"
-              placeholder="Enter the email address"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
+//           <div className="input-container">
+//             <label className="input-label">Email address</label>
+//             <input
+//               type="email"
+//               className="input-field"
+//               placeholder="Enter the email address"
+//               value={email}
+//               onChange={(e) => setEmail(e.target.value)}
+//             />
+//           </div>
 
-          <div className="input-container">
-            <label className="input-label">Password</label>
-            <input
-              type="password"
-              className="input-field"
-              placeholder="Enter the password"
-              value={password}
-              onChange={(e) => setPW(e.target.value)}
-            />
-          </div>
-          <div className="cu-button-container">
+//           <div className="input-container">
+//             <label className="input-label">Password</label>
+//             <input
+//               type="password"
+//               className="input-field"
+//               placeholder="Enter the password"
+//               value={password}
+//               onChange={(e) => setPW(e.target.value)}
+//             />
+//           </div>
+//           <div className="cu-button-container">
 
-            <button className="cu-button" type="submit" onClick={() => createUser()}>
-              Create
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
-}
-export default Create;
+//             <button className="cu-button" type="submit" onClick={() => createUser()}>
+//               Create
+//             </button>
+//           </div>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// }
+// export default Create;
 
-// createUser function = call createClient in the backend
+// // createUser function = call createClient in the backend
 
--- client/durandaltest/src/Pages/Update.js
+++ client/durandaltest/src/Pages/Update.js
@@ -1,128 +1,128 @@
-import React, { useState } from "react";
-// import { useNavigate } from "react-router-dom";
-import axios from "axios";
-import NavBar from "../Components/Navbar";
+// import React, { useState } from "react";
+// // import { useNavigate } from "react-router-dom";
+// import axios from "axios";
+// import NavBar from "../Components/Navbar";
 
-// const token = localStorage.getItem("token")
+// // const token = localStorage.getItem("token")
 
-// axios.interceptors.request.use(
-//   config => {
-//     config.headers.authorization = `Bearer ${token}`;
+// // axios.interceptors.request.use(
+// //   config => {
+// //     config.headers.authorization = `Bearer ${token}`;
+// //   }
+// // )
+
+
+// const Update = () => {
+//   // const navigate = useNavigate();
+//   //const [userName, setUser] = useState("");
+//   const [email, setEmail] = useState("");
+//   const [name, setName] = useState("");
+//   const [firstSurname, setFirstSurname] = useState("");
+//   const [secondSurname, setSecondSurname] = useState("");
+//   const [password, setPW] = useState("");
+
+//   const updateUser = () => {
+//     axios.patch('http://localhost:3001/update', { // url to POST
+//       'Authorization': "bearer " + localStorage.getItem('token'),
+//       //user_name: userName,
+//       name: name,
+//       first_surname: firstSurname,
+//       second_surname: secondSurname,
+//       email: email,
+//       password: password
+
+
+//     },)
+//       .then((response) => {
+//         console.log(response);
+//         alert("Usuario actualizado exitosamente en la base de datos.");
+//       }, (error) => {
+//         console.log(error);
+//         alert("Error al actualizar datos del usuario. Vuelve a intentarlo.");
+//       });
+//     // navigate('/signin', { state: { name: name } });
 //   }
-// )
 
+//   /*
+//   const onSuccess = (res) => {
+//     console.log('success:', res);
+//   };
+//   const onFailure = (err) => {
+//     console.log('failed:', err);
+//   };
+//   */
 
-const Update = () => {
-  // const navigate = useNavigate();
-  //const [userName, setUser] = useState("");
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const [firstSurname, setFirstSurname] = useState("");
-  const [secondSurname, setSecondSurname] = useState("");
-  const [password, setPW] = useState("");
+//   return (
+//     <div className="cu-container">
+//       <NavBar />
+//       <div className="cu-content-container">
+//         <form className="cu-form">
+//           <h1 className="title">DABI Durandal | Update - Users</h1>
+//           <div className="input-container">
+//             <label className="input-label">Name</label>
+//             <input
+//               type="text"
+//               className="input-field"
+//               placeholder="e.g Miguel"
+//               value={name}
+//               onChange={(e) => setName(e.target.value)}
+//             />
+//           </div>
 
-  const updateUser = () => {
-    axios.patch('http://localhost:3001/update', { // url to POST
-      'Authorization': "bearer " + localStorage.getItem('token'),
-      //user_name: userName,
-      name: name,
-      first_surname: firstSurname,
-      second_surname: secondSurname,
-      email: email,
-      password: password
+//           <div className="input-container">
+//             <label className="input-label">First Surname</label>
+//             <input
+//               type="text"
+//               className="input-field"
+//               placeholder="e.g Bermea"
+//               value={firstSurname}
+//               onChange={(e) => setFirstSurname(e.target.value)}
+//             />
+//           </div>
 
+//           <div className="input-container">
+//             <label className="input-label">Second Surname</label>
+//             <input
+//               type="text"
+//               className="input-field"
+//               placeholder="e.g Rodriguez"
+//               value={secondSurname}
+//               onChange={(e) => setSecondSurname(e.target.value)}
+//             />
+//           </div>
 
-    },)
-      .then((response) => {
-        console.log(response);
-        alert("Usuario actualizado exitosamente en la base de datos.");
-      }, (error) => {
-        console.log(error);
-        alert("Error al actualizar datos del usuario. Vuelve a intentarlo.");
-      });
-    // navigate('/signin', { state: { name: name } });
-  }
+//           <div className="input-container">
+//             <label className="input-label">Email address</label>
+//             <input
+//               type="email"
+//               className="input-field"
+//               placeholder="Enter the new email address"
+//               value={email}
+//               onChange={(e) => setEmail(e.target.value)}
+//             />
+//           </div>
 
-  /*
-  const onSuccess = (res) => {
-    console.log('success:', res);
-  };
-  const onFailure = (err) => {
-    console.log('failed:', err);
-  };
-  */
-
-  return (
-    <div className="cu-container">
-      <NavBar />
-      <div className="cu-content-container">
-        <form className="cu-form">
-          <h1 className="title">DABI Durandal | Update - Users</h1>
-          <div className="input-container">
-            <label className="input-label">Name</label>
-            <input
-              type="text"
-              className="input-field"
-              placeholder="e.g Miguel"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-          </div>
-
-          <div className="input-container">
-            <label className="input-label">First Surname</label>
-            <input
-              type="text"
-              className="input-field"
-              placeholder="e.g Bermea"
-              value={firstSurname}
-              onChange={(e) => setFirstSurname(e.target.value)}
-            />
-          </div>
-
-          <div className="input-container">
-            <label className="input-label">Second Surname</label>
-            <input
-              type="text"
-              className="input-field"
-              placeholder="e.g Rodriguez"
-              value={secondSurname}
-              onChange={(e) => setSecondSurname(e.target.value)}
-            />
-          </div>
-
-          <div className="input-container">
-            <label className="input-label">Email address</label>
-            <input
-              type="email"
-              className="input-field"
-              placeholder="Enter the new email address"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-
-          <div className="input-container">
-            <label className="input-label">Password</label>
-            <input
-              type="password"
-              className="input-field"
-              placeholder="Enter the new password"
-              value={password}
-              onChange={(e) => setPW(e.target.value)}
-            />
-          </div>
-          <div className="cu-button-container">
-            <button className="cu-button" type="submit" onClick={() => updateUser()}>
-              Update
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
-}
-export default Update;
+//           <div className="input-container">
+//             <label className="input-label">Password</label>
+//             <input
+//               type="password"
+//               className="input-field"
+//               placeholder="Enter the new password"
+//               value={password}
+//               onChange={(e) => setPW(e.target.value)}
+//             />
+//           </div>
+//           <div className="cu-button-container">
+//             <button className="cu-button" type="submit" onClick={() => updateUser()}>
+//               Update
+//             </button>
+//           </div>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// }
+// export default Update;
 
 
 
